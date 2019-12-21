721f33b42742a86a2addc70e278beadf45095eb8
M Rajesh <m.rajeshrajz@gmail.com>
2019-12-21 11:20:26 +0530

    service worker updated

--- sw.js
+++ sw.js
@@ -22,28 +22,28 @@
 'use strict';
 
 self.addEventListener('push', function (event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-    const title = 'Push Codelab';
-    const options = {
-        body: 'Yay it works.',
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
-    };
+  const title = 'Push Codelab';
+  const options = {
+    body: "Message : " + `${event.data.text()}`,
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
 
-    const notificationPromise = self.registration.showNotification(title, options);
-    event.waitUntil(notificationPromise);
+  const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
 });
 
 
 
-self.addEventListener('notificationclick', function(event) {
-    console.log('[Service Worker] Notification click Received.');
-  
-    event.notification.close();
-  
-    event.waitUntil(
-      clients.openWindow('https://developers.google.com/web/')
-    );
-  });
+self.addEventListener('notificationclick', function (event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
